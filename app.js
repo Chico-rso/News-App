7c787995768eabff821d17551fedfc717468def3
Amiran <chicobes1@gmail.com>
2022-03-26 14:25:48 +0300

    added render news

--- app.js
+++ app.js
@@ -1,62 +1,122 @@
 // Custom Http Module
 function customHttp() {
-  return {
-    get(url, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.addEventListener('load', () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
-          }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
+return {
+	get(url, cb) {
+	try {
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.addEventListener('load', () => {
+		if (Math.floor(xhr.status / 100) !== 2) {
+			cb(`Error. Status code: ${xhr.status}`, xhr);
+			return;
+		}
+		const response = JSON.parse(xhr.responseText);
+		cb(null, response);
+		});
 
-        xhr.addEventListener('error', () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
+		xhr.addEventListener('error', () => {
+		cb(`Error. Status code: ${xhr.status}`, xhr);
+		});
 
-        xhr.send();
-      } catch (error) {
-        cb(error);
-      }
-    },
-    post(url, body, headers, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.addEventListener('load', () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
-          }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
+		xhr.send();
+	} catch (error) {
+		cb(error);
+	}
+	},
+	post(url, body, headers, cb) {
+	try {
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', url);
+		xhr.addEventListener('load', () => {
+		if (Math.floor(xhr.status / 100) !== 2) {
+			cb(`Error. Status code: ${xhr.status}`, xhr);
+			return;
+		}
+		const response = JSON.parse(xhr.responseText);
+		cb(null, response);
+		});
 
-        xhr.addEventListener('error', () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
+		xhr.addEventListener('error', () => {
+		cb(`Error. Status code: ${xhr.status}`, xhr);
+		});
 
-        if (headers) {
-          Object.entries(headers).forEach(([key, value]) => {
-            xhr.setRequestHeader(key, value);
-          });
-        }
+		if (headers) {
+		Object.entries(headers).forEach(([key, value]) => {
+			xhr.setRequestHeader(key, value);
+		});
+		}
 
-        xhr.send(JSON.stringify(body));
-      } catch (error) {
-        cb(error);
-      }
-    },
-  };
+		xhr.send(JSON.stringify(body));
+	} catch (error) {
+		cb(error);
+	}
+	},
+};
 }
 // Init http module
 const http = customHttp();
 
+const newsService = (function () {
+	const apiKey = '9b9c0b66703746e2b10a96aee825c85e';
+	const apiUrl = 'https://newsapi.org/v2';
+
+	return {
+		topHeadlines(country = 'ru', cb) {
+			http.get(`${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,cb)
+		},
+		everything(query, cb) {
+			http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`,cb)
+		}
+	}
+})();
+
 //  init selects
-document.addEventListener('DOMContentLoaded', function() {
-  M.AutoInit();
+document.addEventListener('DOMContentLoaded', function()
+{
+	M.AutoInit();
+	loadNews();
 });
+
+//load news
+function loadNews()
+{
+	newsService.topHeadlines('ru', onGetResponse);
+}
+//func on get response on server
+function onGetResponse(error, response)
+{
+	renderNews(response.articles);
+}
+// func render news
+function renderNews(news)
+{
+	const newsContainer = document.querySelector('.news-container .row');
+	let fragment = '';
+	news.forEach((newsItem) =>
+	{
+		const el = newsTemplate(newsItem);
+		fragment += el
+	})
+
+	newsContainer.insertAdjacentHTML('afterbegin', fragment)
+}
+//func template news
+function newsTemplate({urlToImage, title, description, url})
+{
+	return `
+		<div class="col s12">
+			<div class="card">
+				<div class="card-image">
+					<img src="${urlToImage}"/>
+					<span class="card-title">${title || ''}</span>
+				</div>
+				<div class="card-content">
+					<p>${description || ''}</p>
+				</div>
+				<div class="card-action">
+					<a href="${url}">Read more</a>
+				</div>
+			</div>
+		</div>
+	`;
+}
